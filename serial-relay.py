2817fcde8f37f0dee9604e08cccbc5dd100cd552
Zoe <github@z303.org>
2020-10-14 22:24:28 +0100

    Remove status

--- serial-relay.py
+++ serial-relay.py
@@ -5,8 +5,9 @@ import argparse
 
 baudrate = 9600
 
-def relay_write(relay, state):
-    command = 0xA0
+setstate = 0xA0
+
+def relay_write(command, relay, state):
     channel = relay + 1
     check = command + channel + state
     message = [command, channel, state, check]
@@ -16,7 +17,7 @@ def relay_write(relay, state):
 parser = argparse.ArgumentParser()
 parser.add_argument("port", help="The port the relay is connected to")
 parser.add_argument("channel", type=int, help="The channel to control")
-parser.add_argument("operation", help="Which operation to execute: open, close or status")
+parser.add_argument("operation", help="Which operation to execute: open or close")
 parser.parse_args()
 
 args = parser.parse_args()
@@ -30,12 +31,10 @@ channel = args.channel
 ser = serial.Serial(port, baudrate, timeout=1)
 
 if operation == "open":
-    relay_write(channel, 1)
+    relay_write(setstate, channel, 1)
 elif operation == "close":
-    relay_write(channel, 0)
-elif operation == "status":
-    print("Not implemented")   
+    relay_write(setstate, channel, 0)
 else:
-    print("Unknown option: Only NC, NO or status are supported")
+    print("Unknown option: Only open or close are supported")
 
 ser.close()
