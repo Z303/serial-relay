7ee26167fbf8592d445033715e89987b8037a724
Zoe <github@z303.org>
2020-10-14 21:43:02 +0100

    Add support for multiple relays

--- serial-relay.py
+++ serial-relay.py
@@ -6,8 +6,9 @@ import argparse
 
 baudrate = 9600
 
-def relay_write(channel, state):
+def relay_write(relay, state):
     command = 0xA0
+    channel = relay + 1
     check = command + channel + state
     message = [command, channel, state, check]
 
@@ -15,6 +16,7 @@ def relay_write(channel, state):
 
 parser = argparse.ArgumentParser()
 parser.add_argument("port", help="The port the relay is connected to")
+parser.add_argument("channel", type=int, help="The channel to control")
 parser.add_argument("operation", help="Which operation to execute: open, close or status")
 parser.parse_args()
 
@@ -24,12 +26,14 @@ args = parser.parse_args()
 port = args.port.lower()
 operation = args.operation.lower()
 
+channel = args.channel
+
 ser = serial.Serial(port, baudrate, timeout=1)
 
 if operation == "open":
-    relay_write(1, 1)
+    relay_write(channel, 1)
 elif operation == "close":
-    relay_write(1, 0)
+    relay_write(channel, 0)
 elif operation == "status":
     print("Not implemented")   
 else:
