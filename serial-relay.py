658ae026218917d252145546acc2ae412762f190
Zoe <github@z303.org>
2020-10-14 20:46:40 +0100

    Add command line options

--- serial-relay.py
+++ serial-relay.py
@@ -2,16 +2,31 @@
 
 import time
 import serial
+import argparse
 
-port = 'COM3'
+relayon = [0xA0, 0x01, 0x01, 0xA2]
+relaync = [0xA0, 0x01, 0x00, 0xA1]
+
+parser = argparse.ArgumentParser()
+parser.add_argument("port", help="The port the relay is connected to")
+parser.add_argument("operation", help="Which operation to execute, NC, NO or status")
+parser.parse_args()
+
+args = parser.parse_args()
+
+# case insensitive
+port = args.port.lower()
+operation = args.operation.lower()
+
 ser = serial.Serial(port, 9600, timeout=1)
 
-turnon = [0xA0, 0x01, 0x01, 0xA2]
-turnoff = [0xA0, 0x01, 0x00, 0xA1]
+if operation == "no":
+    ser.write(relayon)
+elif operation == "nc":
+    ser.write(relaync)
+elif operation == "status":
+    print("Not implemented")   
+else:
+    print("Unknown option: Only NC, NO or status are supported")
 
-ser.write(turnon)
-print('ON')
-time.sleep(1)
-ser.write(turnoff)
-print('OFF')
 ser.close()
